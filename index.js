7aa01e2cd8c5b825bf1442f0f8b6b480ec63adfc
Huy Duc Do <huyducdo@googlemail.com>
2019-11-22 09:04:14 +0100

    Hexcode zur Verfügung gestellt

--- index.js
+++ index.js
@@ -12,13 +12,9 @@ let textArrayAnimals = textAnimals.split("\n");
 
 var textColors = fs.readFileSync("./colors.txt").toString('utf-8');
 let textArrayColors = textColors.split("\n");
-textArrayColors.forEach(element => console.log(element));
 
-
-console.log(textColors);
-console.log(textArrayColors);
-
-let textArrayColorsHex = textColors.split("\n");
+var textColorsHex = fs.readFileSync("./colors-hex.txt").toString('utf-8');
+let textArrayColorsHex = textColorsHex.split("\n");
 
 // Socket Stuff
 io.on('connection', (socket) => {
@@ -35,13 +31,15 @@ io.on('connection', (socket) => {
     // });
 
     socket.on('get-name', () => {
-        let randomColor = textArrayColors[Math.floor(Math.random() * textArrayColors.length)];
         let randomAnimal = textArrayAnimals[Math.floor(Math.random() * textArrayAnimals.length)]
+        let randomNumber = Math.floor(Math.random() * textArrayColors.length);
+        let randomColor = textArrayColors[randomNumber];
+        let randomColorHex = textArrayColorsHex[randomNumber];
         let createdAt = moment(new Date()).format('MMMM Do YYYY, h:mm:ss a'); // November 21st 2019, 2:50:29 pm
         let userName = randomColor + " " + randomAnimal;
         console.log(createdAt + ": " + userName);
         socket.username = userName;
-        socket.emit('username', { randomColor: randomColor, randomAnimal: randomAnimal, userName: userName, createdAt: createdAt });
+        socket.emit('username', { randomColor: randomColor, randomColorHex: randomColorHex, randomAnimal: randomAnimal, userName: userName, createdAt: createdAt });
         io.emit('users-changed', { user: userName, event: 'joined' });
     });
 
@@ -52,7 +50,7 @@ io.on('connection', (socket) => {
 });
 
 // Express Server
-var port = process.env.PORT || 3001;
+var port = process.env.PORT || 3002;
 
 server.listen(port, function() {
     console.log('listening in http://localhost:' + port);
