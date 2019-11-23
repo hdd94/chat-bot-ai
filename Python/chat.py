4328f29e77326c91955479bc8650e3686a42788e
Robin Courage <>
2019-11-23 12:31:31 +0100

    new path

--- Python/chat.py
+++ Python/chat.py
@@ -2,12 +2,10 @@ from chatterbot.trainers import ListTrainer
 from chatterbot import ChatBot
 import os
 import sys
-
+sys.prefix ="virtualenv/lib/python3.7"
 bot = ChatBot('Test')
-
 def chat():
     return bot.get_response(sys.argv[1])
-
 if __name__ == '__main__':
     var = chat()
     print(var)
--- Python/init.py
+++ Python/init.py
@@ -1,28 +1,20 @@
 from chatterbot.trainers import ListTrainer
 from chatterbot import ChatBot
 import os
+import sys
 import nltk
 nltk.download()
-
+sys.prefix = "virtualenv/lib/python3.7"
 bot = ChatBot('Test')
-
 def init():
     for file in os.listdir("Python/Training"):
         if file.endswith(".txt"):
             conv = open('Python/Training/' + file, 'r').readlines()
-
     trainer = ListTrainer(bot)
-
     trainer.train(conv)
     return("Chats loaded")
-
 def chat():
     return bot.get_response("message")
-
 if __name__ == '__main__':
     var = init()
     print(var)
-
-
-
-
